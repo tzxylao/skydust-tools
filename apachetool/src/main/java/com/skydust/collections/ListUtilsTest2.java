f80167a72bb751a2f09037b3f02b29c6bca3ab16
laoliangliang1752 <15068610616@163.com>
2017-05-26 15:32:09 +0800

    测试值传递和引用传递，测试结果bigDecimal为值传递，String为值传递，基本类型为值传递。其他对象一般都为引用传递

--- apachetool/src/main/java/com/skydust/collections/ListUtilsTest2.java
+++ apachetool/src/main/java/com/skydust/collections/ListUtilsTest2.java
@@ -3,6 +3,7 @@ package com.skydust.collections;
 import com.skydust.bean.Subject;
 import org.apache.commons.collections.ListUtils;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,22 +15,28 @@ public class ListUtilsTest2 {
         List<Subject> list = new ArrayList<>();
         Subject subject = new Subject();
         subject.setSubject_id("1");
+        subject.setRaise_surplus_amount(new BigDecimal("10"));
         list.add(subject);
         subject = new Subject();
         subject.setSubject_id("2");
+        subject.setRaise_surplus_amount(new BigDecimal("11"));
         list.add(subject);
         subject = new Subject();
         subject.setSubject_id("3");
+        subject.setRaise_surplus_amount(new BigDecimal("12"));
         list.add(subject);
         List<Subject> list2 = new ArrayList<>();
         subject = new Subject();
         subject.setSubject_id("1");
+        subject.setRaise_surplus_amount(new BigDecimal("13"));
         list2.add(subject);
         subject = new Subject();
         subject.setSubject_id("2");
+        subject.setRaise_surplus_amount(new BigDecimal("14"));
         list2.add(subject);
         subject = new Subject();
         subject.setSubject_id("4");
+        subject.setRaise_surplus_amount(new BigDecimal("15"));
         list2.add(subject);
         List list3 = ListUtils.removeAll(list, list2);
         for (Object o : list3) {
---
+++ apachetool/src/main/java/com/skydust/random/methodtest.java
@@ -0,0 +1,72 @@
+package com.skydust.random;
+
+import com.skydust.bean.Apple;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by laoliangliang on 2017/5/25.
+ */
+public class methodtest {
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        delelteList(list);
+        for (Object o : list) {
+            System.out.print(o+" ");
+        }
+
+        List<String> mylist = new ArrayList<>();
+        mylist.add("e");
+        changeMyList(mylist);
+        for (String s : mylist) {
+            System.out.println(s);
+        }
+
+        BigDecimal decimal = BigDecimal.ZERO;
+        changeBigDecimal(decimal);
+
+        Apple apple = new Apple();
+        apple.setColor("red");
+        apple.setId("1");
+        changeApple(apple);
+        System.out.println(apple);
+
+        String bar = new String("123");
+        changeString(bar);
+    }
+
+    private static void changeString(String bar) {
+        bar = "456";
+    }
+
+    private static void changeApple(Apple apple) {
+        apple.setColor("green");
+        apple.setId("2");
+    }
+
+    private static void changeBigDecimal(BigDecimal decimal) {
+        decimal = decimal.add(BigDecimal.ONE);
+    }
+
+    private static void changeMyList(List<String> mylist) {
+        mylist.add("h");
+    }
+
+    public static void change(BigDecimal amount,Integer n){
+        amount = amount.add(BigDecimal.ONE);
+        n = new Integer("2");
+    }
+
+    public static void delelteList(List list){
+        for (Iterator iterator = list.iterator(); iterator.hasNext(); ) {
+            Object next =  iterator.next();
+//            iterator.remove();
+        }
+    }
+}
